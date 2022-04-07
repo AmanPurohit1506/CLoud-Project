37cc0ac2697d8249a2c6f9cd4af1449dfa0ea03f
Aman Purohit <30374827+AmanPurohit1506@users.noreply.github.com>
2022-04-07 09:00:06 +0530

    Update app-a2.py

--- c756-term-project/s2/standalone/app-a2.py
+++ c756-term-project/s2/standalone/app-a2.py
@@ -19,7 +19,7 @@ from flask import request
 import unique_code
 
 # The path to the file (CSV format) containing the sample data
-DB_PATH = '/data/music.csv'
+DB_PATH = 'music.csv'
 
 # The unique exercise code
 # The EXER environment variable has a value specific to this exercise
